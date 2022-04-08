41df5d5763ea57904fe5d3e2563e89d17cafa7b6
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-04-08 16:01:10 +0100

    class

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
+/* 
+=> all’interno della classe è definito almeno un metodo
+vengono istanziati almeno due oggetti ‘Movie’
+stampate a schermo i valori delle relative proprietà di ogni oggetto (potete creare un array con dentro le due istanze e fare un ciclo per stampare i dati) */
+
+class Movie {
+
+    public $img;
+    public $title;
+    public $language;
+    public $genre;
+    public $director;
+
+    function __construct($_img, string $_title, string $_language, string $_genre, string $_director){
+
+        $this->img = $_img;
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->genre = $_genre;
+        $this->director = $_director;
+    }
+}
+
 
 
 ?>
